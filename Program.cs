68c986c5e8bcf2caaf741f4a8caf1a31fda2b26e
Martin Larsson <martin.larsson@umea.nti.se>
2015-11-23 14:32:50 +0100

    project init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Phidgets;
+using Phidgets.Events;
+
+namespace Itrash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
