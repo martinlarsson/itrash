8c028ba9c692f75cda49b99da995acac1e3f770e
Martin Larsson (dv13mln) <dv13mln@cs.umu.se>
2015-11-24 16:04:12 +0100

    Testing InterfaceKit

--- Itrash.cs
+++ Itrash.cs
@@ -17,42 +17,8 @@ namespace Itrash
         {
             try
             {
-                servo = new Servo();
-                servo.Attach += new AttachEventHandler(servo_Attach);
-                servo.Detach += new DetachEventHandler(servo_Detach);
-                servo.Error += new ErrorEventHandler(servo_Error);
-                servo.PositionChange += new PositionChangeEventHandler(servo_PositionChange);
-                servo.open();
-                Console.WriteLine("Awaiting Servo attachment..");
-                servo.waitForAttachment();
-                if (servo.Attached)
-                {
-                    servo.servos[0].Position = 15.00; 
-                }
-                Console.WriteLine("Servo's position set to 0.00");
-                Console.WriteLine("Press any key to continue...");
-                Console.Read();
-                if (servo.Attached)
-                {
-                    for (double i = 0.00; i < 110.00; i++)
-                    {
-                        Thread.Sleep(10);
-                        servo.servos[0].Position = i;
-                    }
-                }
-                servo.close();
-                servo = null;
-
-                /*
-                ifKit = new InterfaceKit();
-                ifKit.Attach += new AttachEventHandler(ifKit_Attach);
-                ifKit.Detach += new DetachEventHandler(ifKit_Detach);
-                ifKit.Error += new ErrorEventHandler(ifKit_Error);
-                ifKit.InputChange += new InputChangeEventHandler(ifKit_InputChange);
-                ifKit.OutputChange += new OutputChangeEventHandler(ifKit_OutputChange);
-                ifKit.SensorChange += new SensorChangeEventHandler(ifKit_SensorChange);
-                ifKit = null;
-                */
+                initInterFaceKit();
+                //initServo();
             }
             catch (PhidgetException pex)
             {
@@ -60,49 +26,110 @@ namespace Itrash
             }
         }
 
+        private void initInterFaceKit()
+        {
+            // Initialize the InterfaceKit object
+            ifKit = new InterfaceKit();
+
+            // Hook the basic event-handlers
+            ifKit.Attach += new AttachEventHandler(ifKit_Attach);
+            ifKit.Detach += new DetachEventHandler(ifKit_Detach);
+            ifKit.Error += new ErrorEventHandler(ifKit_Error);
+
+            // Hook the InterfaceKit event-handlers
+            ifKit.InputChange += new InputChangeEventHandler(ifKit_InputChange);
+            ifKit.OutputChange += new OutputChangeEventHandler(ifKit_OutputChange);
+            ifKit.SensorChange += new SensorChangeEventHandler(ifKit_SensorChange);
+
+            // Open the object for device connections
+            ifKit.open();
+
+            // Wait for an InterfaceKit phidget to be attached
+            Console.WriteLine("Press any key to end...");
+            Console.Read();
+
+            // User input was read so we'll terminate the program, so close the object
+            ifKit.close();
+
+            // Set the object to null to get it out of memory
+            ifKit = null;
+        }
+
+        private void initServo()
+        {
+            servo = new Servo();
+            servo.Attach += new AttachEventHandler(servo_Attach);
+            servo.Detach += new DetachEventHandler(servo_Detach);
+            servo.Error += new ErrorEventHandler(servo_Error);
+            servo.PositionChange += new PositionChangeEventHandler(servo_PositionChange);
+            servo.open();
+            Console.WriteLine("Awaiting Servo attachment..");
+            servo.waitForAttachment();
+            if (servo.Attached)
+            {
+                servo.servos[0].Position = 15.00;
+            }
+            Console.WriteLine("Servo's position set to 15.00");
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+            if (servo.Attached)
+            {
+                servo.servos[0].Position = 115;
+            }
+
+
+
+            servo.close();
+            servo = null;
+        }
+        /// <summary>
+        /// Du kan va en summary!
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void servo_PositionChange(object sender, PositionChangeEventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Servo {0} Position {1}", e.Index, e.Position);
         }
 
         private void servo_Error(object sender, ErrorEventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine(e.Description);
         }
 
         private void servo_Detach(object sender, DetachEventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Detached"+e.Device);
         }
 
         private void servo_Attach(object sender, AttachEventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Attached "+e.Device);
         }
 
-        public void run()
-        {
-            ifKit.open();
-            Console.WriteLine("Awaiting Interface Kit attachment...");
-            ifKit.waitForAttachment();
-            Console.WriteLine("Nr of sensors: " + ifKit.sensors.Count);
-            ifKit.close();
-            ifKit = null;
-        }
+        //public void run()
+        //{
+           // ifKit.open();
+            //Console.WriteLine("Awaiting Interface Kit attachment...");
+            //ifKit.waitForAttachment();
+            //Console.WriteLine("Nr of sensors: " + ifKit.sensors.Count);
+            //ifKit.close();
+            //ifKit = null;
+        //}
 
         private void ifKit_SensorChange(object sender, SensorChangeEventArgs e)
         {
-            Console.WriteLine("Sensor index {0} value {1}", e.Index, e.Index);
+            Console.WriteLine("Sensor index {0} value {1}", e.Index, e.Value.ToString());
         }
 
         private void ifKit_OutputChange(object sender, OutputChangeEventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Output index {0} value {1}", e.Index, e.Value.ToString());
         }
 
         private void ifKit_InputChange(object sender, InputChangeEventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Input index {0} value {1}", e.Index, e.Value.ToString());
         }
 
         private void ifKit_Error(object sender, ErrorEventArgs e)
