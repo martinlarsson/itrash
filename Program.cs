715456888cee66aa28b854f0bf742fa8d19fc747
dv13esn <dv13esn@cs.umu.se>
2015-11-24 12:46:29 +0100

    Initialized Form1. Created method stubds for event handlers.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Phidgets;
-using Phidgets.Events;
+using System.Windows.Forms;
 
 namespace Itrash
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("En apa och en katt, vilken underbar skatt!");
+            //Console.WriteLine("En apa och en katt, vilken underbar skatt!");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1Test());
         }
     }
 }
